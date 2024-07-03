20b2d6e648643dc08d464448f917ea09bbd1f7a0
sumit9011 <141544733+sumit9011@users.noreply.github.com>
2024-07-03 23:03:57 +0530

    Time: 4 ms (31.12%), Space: 9.8 MB (36.21%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0;
+        int r=nums.size()-1;
+        for(int i=0;i<nums.size() && i<=r;)
+        {
+            if(nums[i]==0)
+            {
+                swap(nums[i],nums[l]);
+                l++;
+                i++;
+            }
+            else if(nums[i]==2)
+            {
+                swap(nums[i],nums[r]);
+                r--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        
+    }
+};
