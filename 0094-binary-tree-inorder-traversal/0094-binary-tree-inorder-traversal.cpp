cbf4efdd11764e2fbac76f3d63a3bd735a4abf5d
sumit9011 <141544733+sumit9011@users.noreply.github.com>
2024-07-18 19:39:18 +0530

    Time: 0 ms (100%), Space: 10.1 MB (16.55%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,23 +11,53 @@
  */
 class Solution {
 public:
-
-    void Inorder(TreeNode* root,vector<int> &ans)
-    {
-        if(root==NULL)
-        return;
-
-        Inorder(root->left,ans);
-        ans.push_back(root->val);
-        Inorder(root->right,ans);
-
-    }
-
     vector<int> inorderTraversal(TreeNode* root) {
+        
+            //morris traversal
 
         vector<int> ans;
-        Inorder(root,ans);
+
+        if(root==NULL)
         return ans;
-        
+
+        TreeNode* curr=root;
+
+        while(curr!=NULL)
+        {
+            if(curr->left==NULL)
+            {
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+
+            else
+            {
+                //step 1 : find predecessor node of curr
+
+                TreeNode* pre=curr->left;
+                while(pre->right!=NULL && pre->right!=curr)
+                {
+                    pre=pre->right;
+                }
+
+
+                //step 2 : if pred->right==NULL
+                if(pre->right==NULL)
+                {
+                    pre->right=curr;
+                    curr=curr->left;
+                }
+
+                //step 3 : if pred->right==curr
+                else
+                {
+                    pre->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+
+        return ans;
     }
 };
