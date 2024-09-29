3cbbf13981595cdceb892888b50cec74332c8868
sumit9011 <141544733+sumit9011@users.noreply.github.com>
2024-09-29 18:57:04 +0530

    Time: 16 ms (67.34%), Space: 27 MB (49.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,21 +2,40 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        //approach-2
-        map<int,int> mp;
-        int maj=nums.size()/2;
+        //approach-3 Moore's voting algo
+        
+        int cnt1=0;
+        int el;
+        int n=nums.size();
 
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            mp[nums[i]]++;
+            if(cnt1==0)
+            {
+                el=nums[i];
+                cnt1=1;
+            }
+
+            else if(nums[i]==el)
+            cnt1++;
+
+            else
+            cnt1--;
         }
 
-        for(auto x: mp)
+        //element stored in el can be or not be a majority element
+        //hence check it
+
+        
+        int cnt2=0;
+
+        for(int i=0;i<n;i++)
         {
-            if(x.second > maj)
-            return x.first;
+            if(nums[i]==el)
+            cnt2++;
         }
 
-        return -1;
+        if(cnt2>(n/2)) return el;
+        else return -1;
     }
 };
