93fbb5e7c5e0b418761dfa9d3dca1c337311c819
sumit9011 <141544733+sumit9011@users.noreply.github.com>
2025-02-06 10:51:05 +0530

    Time: 0 ms (100%), Space: 77.1 MB (87.96%) - LeetHub

--- 2684-determine-the-winner-of-a-bowling-game/2684-determine-the-winner-of-a-bowling-game.cpp
+++ 2684-determine-the-winner-of-a-bowling-game/2684-determine-the-winner-of-a-bowling-game.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int isWinner(vector<int>& player1, vector<int>& player2) {
+         //Approach 2
 
-        int n=player1.size();
-        int val1=0;
-        int val2=0;
+         int n=player1.size();
+         int val1=0;
+         int val2=0;
+
+         bool bonus1_prev=false;
+         bool bonus1_prev2=false;
+         bool bonus2_prev=false;
+         bool bonus2_prev2=false;
 
         for(int i=0;i<n;i++)
         {
-            if((i-1>=0 && player1[i-1]==10) || (i-2>=0 && player1[i-2]==10))
-            {
-                val1+=2*player1[i];
-            }
-            else val1+=player1[i];
+            int multiplier1=(bonus1_prev || bonus1_prev2)?2:1;
+            int multiplier2=(bonus2_prev || bonus2_prev2)?2:1;
 
+            val1+=multiplier1* player1[i];
+            val2+=multiplier2* player2[i];
 
-            if((i-1>=0 && player2[i-1]==10) || (i-2>=0 && player2[i-2]==10))
-            {
-                val2+=2*player2[i];
-            }
-            else val2+=player2[i];
+            bonus1_prev2=bonus1_prev;
+            bonus1_prev=(player1[i]==10);
 
+            bonus2_prev2=bonus2_prev;
+            bonus2_prev=(player2[i]==10);
         }
 
         if(val1>val2) return 1;
-        else if(val1<val2) return 2;
+        else if(val2>val1) return 2;
         else return 0;
 
     }
